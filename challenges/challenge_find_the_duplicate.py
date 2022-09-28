7a6e1a3cbdd01ecc3532edf954b5cc09feb01249
RuanCunha <ruanvcunha@hotmail.com>
2022-09-28 02:47:39 -0300

    Find Duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,46 @@
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)
+
+    return numbers
+
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]
+    right = numbers[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+
+    return numbers
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    result = merge_sort(nums)
+    for index, number in enumerate(result[:-1]):
+        if isinstance(number, str):
+            return False
+        if number < 0:
+            return False
+        if number == result[index + 1]:
+            return number
+
+    return False
