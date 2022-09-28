070cc11d1392002e77b673c63400c680bb5ee5df
RuanCunha <ruanvcunha@hotmail.com>
2022-09-28 02:06:25 -0300

    Anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
+def compare_word(first_word, second_word):
+    for letter in first_word:
+        if letter in second_word:
+            second_word = second_word.replace(letter, "", 1)
+        else:
+            return False
+
+    return True
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if len(first_string) != len(second_string):
+        return False
+
+    if first_string == "" or second_string == "":
+        return False
+
+    first_word, second_word = first_string.lower(), second_string.lower()
+
+    return compare_word(first_word, second_word)
